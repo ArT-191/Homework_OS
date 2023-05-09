991b701e7e0ac9c7b2e7f0e929b25e70ef82a337
ArT-191 <115080147+ArT-191@users.noreply.github.com>
2023-05-09 19:16:23 +0400

    Update deinitializer.cpp

--- Transparent_Bank/deinitializer.cpp
+++ Transparent_Bank/deinitializer.cpp
@@ -1,21 +1,13 @@
-#include <sys/ipc.h>
-#include <sys/shm.h>
 #include "bank.h"
+#include <stdlib.h>
 
-int destroy_bank() {
-    key_t shm_key = ftok("bank.h", 'R');
-    if (shm_key == -1) {
-        return 1;
+void destroy_bank(bank_t *bank) {
+    // Free memory used by account array
+    for (size_t i = 0; i < bank->num_accounts; i++) {
+        free(bank->accounts[i]);
     }
+    free(bank->accounts);
 
-    int shm_id = shmget(shm_key, sizeof(bank_t), 0666);
-    if (shm_id < 0) {
-        return 1;
-    }
-
-    if (shmctl(shm_id, IPC_RMID, NULL) < 0) {
-        return 1;
-    }
-
-    return 0;
+    // Free memory used by bank structure
+    free(bank);
 }
